1e5b78a28c135d4ebf9f851e227a618cfe629838
mirsking <mirsking@gmail.com>
2016-01-09 19:50:29 +0800

    add f.lux

---
+++ once_cfg/cfgs/f.lux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo add-apt-repository -y ppa:kilian/f.lux
+sudo apt-get -y update
+sudo apt-get -y install fluxgui
--- once_cfg/shortcuts_cfg.sh
+++ once_cfg/shortcuts_cfg.sh
@@ -10,3 +10,11 @@ $BASH_DIR/cfgs/thunder_cfg.sh
 
 # JDK8u60
 $BASH_DIR/cfgs/jdk_cfg.sh
+
+# SmartGit
+sudo add-apt-repository ppa:eugenesan/ppa
+sudo apt-get update
+
+# pycharm
+sudo rm /usr/bin/pycharm
+sudo ln -s $BASH_DIR/../shortcuts/pycharm /usr/bin/pycharm
